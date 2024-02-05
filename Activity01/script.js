9e337d3b0f45be46d15e8927adc22cd08f2ae9d3
leocmk <158284007+leocmk@users.noreply.github.com>
2024-02-05 09:52:50 -0300

    Add files via upload

---
+++ Activity01/script.js
@@ -0,0 +1,6 @@
+function somar() {
+    var numero1 = document.getElementById("num1").valueAsNumber;
+    var numero2 = document.getElementById("num2").valueAsNumber;
+    var resultado = numero1 + numero2;
+    alert(resultado)
+}
